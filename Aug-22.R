50be260d2a155f3a010b25fdbe3430933e5a05bb
Shamaria Stroman <shamaria.stroman@icloud.com>
2024-08-22 16:25:16 -0400

    Add files via upload

---
+++ Aug-22.R
@@ -0,0 +1,11 @@
+#Author:Shamaria Stroman, Date:08/22/2024, Purpose:Test the measures of descriptive stats
+
+#How to calculate standard deviation in R
+X <- c(1,2,3,4,5)
+sd(X)
+
+#How to calculate mean in R
+mean(X)
+
+#how to calculate median in R
+median(X)
