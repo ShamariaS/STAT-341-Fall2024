9608866e0b5831d7bb4974b399bcb9f3ab31d135
Shamaria Stroman <shamaria.stroman@icloud.com>
2024-09-10 15:43:18 -0400

    Add files via upload

---
+++ Assignment 3.R
@@ -0,0 +1,18 @@
+1. Create 1000 random numbers and store them in a variable called variable1
+2. Calculate SD, mean and median
+3. Save ouput of SD, mean, median in R document along with your statement name and doc
+#Author: Shamaria Stroman, Date:08/29/2024
+
+
+variable1 <- runif(1000)
+sd_variable1 <- sd(variable1)
+mean_variable1 <- mean(variable1)
+median_variable1 <- median(variable1)
+
+#SD
+0.2865432
+#Mean
+0.5026995
+#Median
+0.502073
+
