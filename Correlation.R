f37f21a401d28a12e7e8104241ac83b950c25c2c
Shamaria Stroman <shamaria.stroman@icloud.com>
2024-09-19 15:49:31 -0400

    Add files via upload

---
+++ Correlation.R
@@ -0,0 +1,15 @@
+Author: Shamaria Stroman; Date:September 12, 2024; Purpose: Perform Correlation Analysis
+library("ggpubr")
+
+#loading dummy data for testing
+my_data <- mtcars
+
+#Hypothesis: the weight of the car is inversely proportional to the mpd it travels
+
+
+res<- cor.test(my_data$wt, my_data$mpg, method="pearson")
+
+
+#cor
+#-0.8676594
+#there is a strong inverse relationship between the car's weight and mpg. 
